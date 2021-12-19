c831fd1fefd84d04edf21cd1237a691c788dc4da
hadi <65118892+hadiazt@users.noreply.github.com>
2021-12-19 23:39:50 +0330

    Update removetrusted.js

--- commands/removetrusted.js
+++ commands/removetrusted.js
@@ -1,63 +1,65 @@
 const Discord = require("discord.js")
 const db = require("quick.db")
- const ms = require('parse-ms');
+const ms = require('parse-ms');
 const { truncate } = require("fs");
 module.exports = {
-    name: "removetrusted",
-    description: "set guild anit raid config",
-    run: async (client, message, args) => {
-      const guildicon = message.guild.iconURL();
-if(message.author.id === message.guild.ownerId) {
-    
-        let user = message.mentions.users.first()
-        if(!user) {
-          let usermention = new Discord.MessageEmbed()
+  name: "removetrusted",
+  description: "Set Guild Anit Raid Config",
+  run: async (client, message, args) => {
+    const guildicon = message.guild.iconURL();
+    if (message.author.id === message.guild.ownerId) {
+
+      let user = message.mentions.users.first()
+      if (!user) {
+        let usermention = new Discord.MessageEmbed()
           .setAuthor(message.author.tag, message.author.displayAvatarURL())
           .setDescription(`
           **Mention User!** 
           `)
           .setFooter(message.guild.name, guildicon)
-  
-          return message.channel.send({
-              embeds: [usermention]
-          });
+
+        return message.channel.send({
+          embeds: [usermention]
+        });
       }
 
-        let database = db.get(`trustedusers_${message.guild.id}`)
-        if(database) {
-            let data = database.find(x => x.user === user.id)
-          let unabletofind = new Discord.MessageEmbed()
+      let database = db.get(`trustedusers_${message.guild.id}`)
+      if (database) {
+        let data = database.find(x => x.user === user.id)
+        let unabletofind = new Discord.MessageEmbed()
           .setAuthor(message.author.tag, message.author.displayAvatarURL())
           .setDescription(`
-          ** unable to find that user on database!** 
+          ** Unable To Find That User On DataBase!** 
           `)
           .setFooter(message.guild.name, guildicon)
-          
-            if(!data) return message.channel.send(unabletofind)
-          
-            let value = database.indexOf(data)
-            delete database[value]
-          
-            var filter = database.filter(x => {
-              return x != null && x != ''
-            })
-          
-            db.set(`trustedusers_${message.guild.id}`, filter)
-          let deleted = new Discord.MessageEmbed()
-            .setAuthor(message.author.tag, message.author.displayAvatarURL())
+
+        if (!data) return message.channel.send(unabletofind)
+
+        let value = database.indexOf(data)
+        delete database[value]
+
+        var filter = database.filter(x => {
+          return x != null && x != ''
+        })
+
+        db.set(`trustedusers_${message.guild.id}`, filter)
+        let deleted = new Discord.MessageEmbed()
+          .setAuthor(message.author.tag, message.author.displayAvatarURL())
           .setDescription(`
           **Removed ${user} From Trusted Users!** 
           `)
           .setFooter(message.guild.name, guildicon)
-          
-            return message.channel.send({
-              embeds: [deleted]
-          });
-          
-        } else {          
-     message.channel.send(`that user not on trusted list`)
-        }}
-    
-      message.channel.send(`Only ownership of the guild can use that cmd!`)
-}}
- 
+
+        return message.channel.send({
+          embeds: [deleted]
+        });
+
+      } else {
+        message.channel.send(`That User IS Not On Trusted List`)
+      }
+    }
+
+    message.channel.send(`Only Ownership Of The Guild Can Use That CMD!`)
+  }
+}
+
