6b0e1c580042d65869c485a10a36bec9aade19fa
hadi <65118892+hadiazt@users.noreply.github.com>
2021-12-19 23:40:06 +0330

    Update addtrusted.js

--- commands/addtrusted.js
+++ commands/addtrusted.js
@@ -1,46 +1,47 @@
 const Discord = require("discord.js")
 const db = require("quick.db")
- const ms = require('parse-ms');
+const ms = require('parse-ms');
 const { truncate } = require("fs");
 module.exports = {
     name: "addtrusted",
-    description: "set guild anit raid config",
+    description: "Set Guild Anit Raid Config",
     run: async (client, message, args) => {
-const guildicon = message.guild.iconURL();
-if(message.author.id === message.guild.ownerId) {
-    
-        let user = message.mentions.users.first()
-        if(!user) {
-            let usermention = new Discord.MessageEmbed()
-            .setAuthor(message.author.tag, message.author.displayAvatarURL())
-            .setDescription(`
+        const guildicon = message.guild.iconURL();
+        if (message.author.id === message.guild.ownerId) {
+
+            let user = message.mentions.users.first()
+            if (!user) {
+                let usermention = new Discord.MessageEmbed()
+                    .setAuthor(message.author.tag, message.author.displayAvatarURL())
+                    .setDescription(`
             **Mention User!** 
             `)
-            .setFooter(message.guild.name, guildicon)
-    
-            return message.channel.send({
-                embeds: [usermention]
-            });
-        }
-        let trustedusers = db.get(`trustedusers_${message.guild.id}`)
-        if(trustedusers && trustedusers.find(find => find.user == user.id)) {
-        return message.channel.send(`This User It's Already on trusted list`)
-        }
-let data = {
-    user: user.id
-}
-        db.push(`trustedusers_${message.guild.id}`, data)
-        let added = new Discord.MessageEmbed()
-        .setAuthor(message.author.tag, message.author.displayAvatarURL())
-        .setDescription(`
+                    .setFooter(message.guild.name, guildicon)
+
+                return message.channel.send({
+                    embeds: [usermention]
+                });
+            }
+            let trustedusers = db.get(`trustedusers_${message.guild.id}`)
+            if (trustedusers && trustedusers.find(find => find.user == user.id)) {
+                return message.channel.send(`This User It's Already on Trusted List`)
+            }
+            let data = {
+                user: user.id
+            }
+            db.push(`trustedusers_${message.guild.id}`, data)
+            let added = new Discord.MessageEmbed()
+                .setAuthor(message.author.tag, message.author.displayAvatarURL())
+                .setDescription(`
         **Added ${user} To Trusted List!** 
         `)
-        .setFooter(message.guild.name, guildicon)
+                .setFooter(message.guild.name, guildicon)
 
-        return message.channel.send({
-            embeds: [added]
-        });
+            return message.channel.send({
+                embeds: [added]
+            });
+        }
+        message.channel.send(`Only Ownership Of The Guild Can Use That CMD!`)
     }
-message.channel.send(`Only ownership of the guild can use that cmd!`)
-}}
- 
+}
+
