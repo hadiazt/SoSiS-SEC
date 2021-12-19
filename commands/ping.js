ecf2710cb6014566812ad098ed95962cab0d9a3a
hadi <65118892+hadiazt@users.noreply.github.com>
2021-12-19 23:39:57 +0330

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js")
 module.exports = {
     name: "ping",
-    description: "shows ping",
+    description: "Shows Ping",
 
     run: async (client, message, args) => {
         const embed = new Discord.MessageEmbed()
