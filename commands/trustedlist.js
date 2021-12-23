053629dbe75b121079bfceeeb6bd9277daa35a31
hadi <65118892+hadiazt@users.noreply.github.com>
2021-12-23 21:50:49 +0330

    Finished trustedlist

--- commands/trustedlist.js
+++ commands/trustedlist.js
@@ -5,24 +5,20 @@ const db = require("quick.db")
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('trustedlist')
-    .setDescription('Show The List Of WhiteList Users')
-    .addUserOption(option =>
-      option.setName('user')
-        .setDescription('Mention The User')
-        .setRequired(true)),
+    .setDescription('Show The List Of WhiteList Users'),
   async execute(interaction) {
 
 
     let wordlist = new Discord.MessageEmbed()
       .setTitle(`${interaction.guild.name}'s WhiteList Users :`)
-    let database = db.get(`trustedusers_${message.guild.id}`)
+    let database = db.get(`trustedusers_${interaction.guild.id}`)
     if (database && database.length) {
       let array = []
       database.forEach(m => {
         array.push(`<@${m.user}>`)
       })
 
-      wordlist.description(`${array.join("\n")}`)
+      wordlist.setDescription(array.join('\n\n'))
     }
     return interaction.reply({
       embeds: [wordlist]
