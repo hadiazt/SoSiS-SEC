61e6944113ab3ef607d43760f6184f7f94b0f2cf
hadi <65118892+hadiazt@users.noreply.github.com>
2021-12-24 11:47:32 +0330

    Added actionlogs

--- commands/actionlogs.js
+++ commands/actionlogs.js
@@ -1,22 +1,42 @@
-        // .addChoice('actionlogs', 'actionlogs')
-        // if (cmd.toLowerCase() === 'setactionlogs') {
-        //     let logs = message.mentions.channels.first();
-        //     if (!logs) {
-        //       let logsembed = new Discord.MessageEmbed()
-        //         .setAuthor(message.author.username, message.author.displayAvatarURL())
-        //         .setDescription(`Please Mention an vaild channel`)
-        //         .setFooter(message.guild.name, guildicon)
-        //       return message.channel.send({
-        //         embeds: [logsembed]
-        //       });
-        //     }
-        //     logs.send(`** Anit-Raid Logs Channel **`)
-        //     db.set(`acitonslogs_${message.guild.id}`, logs.id)
-        //     let done = new Discord.MessageEmbed()
-        //       .setAuthor(message.author.username, message.author.displayAvatarURL())
-        //       .setDescription(`well done aciton-logs channel has been set to ${logs}`)
-        //       .setFooter(message.guild.name, guildicon)
-        //     return message.channel.send({
-        //       embeds: [done]
-        //     });
-        //   }
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const Discord = require("discord.js")
+const db = require("quick.db")
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('actionlog')
+        .setDescription('Set Server Aciton Log')
+        .addChannelOption(option =>
+            option.setName('channel')
+                .setDescription('Mention The Channel')
+                .setRequired(true)),
+    async execute(interaction) {
+
+
+        let wluser = db.get(`trustedusers_${interaction.guild.id}`).find(find => find.user == interaction.user.id)
+        if (interaction.user.id === interaction.guild.ownerId || interaction.user.id === wluser) {
+
+            var logs = interaction.options.getChannel('channel')
+
+            db.set(`acitonslogs_${interaction.guild.id}`, logs.id)
+            let done = new Discord.MessageEmbed()
+                .setColor('#85db61')
+                .setDescription(`<:check:923151545401479179> Well Done Aciton-Logs Channel Has Been Set To <#${logs.id}>`)
+            return interaction.reply({
+                embeds: [done]
+            });
+        }
+
+        let owneronly = new Discord.MessageEmbed()
+            .setColor('#f67975')
+            .setTitle(`You Can't Use This Command!`)
+            .setDescription('<:ignore:923151545569267752> Only **Server Owner** & **Trusted Users** Can Use This Command!')
+        return interaction.reply({
+            embeds: [owneronly]
+        });
+    }
+}
+
+
+
+
