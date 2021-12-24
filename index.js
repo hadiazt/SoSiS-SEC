4374e7976d7d64ded1a781c362997f5c7c1dbcea
hadi <65118892+hadiazt@users.noreply.github.com>
2021-12-24 23:54:18 +0330

    Fixed Soem Bugs & Added Logging

--- index.js
+++ index.js
@@ -48,33 +48,44 @@ client.on("roleCreate", async role => {
     const user = await role.guild.fetchAuditLogs({
         type: 'ROLE_CREATE'
     }).then(audit => audit.entries.first())
+
     const entry = user.executor
     let trustedusers = db.get(`trustedusers_${role.guild.id}`)
-    if (trustedusers && trustedusers.find(find => find.user == entry.id)) {
-        return console.log('Its Trusted User');
-    }
-    let author = db.get(`executer_${role.guild.id}_${entry.id}_rolecreate`)
-    let limts = db.get(`rolecreatelimt_${role.guild.id}`)
-    if (limts === null) {
-        return console.log('shit');
-    }
     let logs = db.get(`acitonslogs_${role.guild.id}`)
 
-    if (author > limts) {
-        db.delete(`executer_${role.guild.id}_${entry.id}`)
-        console.log('trying to ban the user..')
+    if (trustedusers && trustedusers.find(find => find.user == entry.id)) {
+        if (logs) {
+            let trustedac = new Discord.MessageEmbed()
+                .setColor('#85db61')
+                .setTitle(`<:check:923151545401479179> ${entry.tag} Created An Role But He/She Is In Trusted Users`)
+            return client.channels.cache.get(logs).send({ embeds: [trustedac] });
+        }
+    }
+
+    let author = db.get(`executer_${role.guild.id}_${entry.id}_rolecreate`)
+    let limts = db.get(`rolecreatelimt_${role.guild.id}`)
+
+    if (limts === null) {
+        let nolimit = new Discord.MessageEmbed()
+            .setColor('#f67975')
+            .setTitle(`<:ignore:923151545569267752> ${entry.tag} Created A Role But No Limit Found To Punish`)
+        return client.channels.cache.get(logs).send({ embeds: [nolimit] });
+    }
+
+    if (author >= limts) {
+        db.delete(`executer_${role.guild.id}_${entry.id}_rolecreate`)
         role.guild.members.ban(entry.id)
         let logsembed = new Discord.MessageEmbed()
-            .setTitle(`${entry.tag} was trying to raid but failed miserabely! [Breaking Roles Create Limts]`)
-        return client.channels.cache.get(logs).send(logsembed);
-
+            .setColor('#00008b')
+            .setTitle(`<:perm:923904697423777792> ${entry.tag} was trying to raid but failed miserabely [Breaking Roles Create Limits]`)
+        return client.channels.cache.get(logs).send({ embeds: [logsembed] });
     }
-    db.add(`executer_${role.guild.id}_${role.guild.id}_rolecreate`, 1)
-    let warn = db.get(`executer_${role.guild.id}_${entry.id}_rolecreate`)
-    let logsembed = new Discord.MessageEmbed()
 
-        .setTitle(`${entry.tag} Is Creating Roles.. [${warn || 0}/${author || 0}]`)
-    client.channels.cache.get(logs).send(logsembed)
+    db.add(`executer_${role.guild.id}_${entry.id}_rolecreate`, 1)
+    let logsembed = new Discord.MessageEmbed()
+        .setColor('RED')
+        .setTitle(`<:erorr:878139495764090880> ${entry.tag} Is Creating Role [${author || 0} /${limts || 0}]`)
+    return client.channels.cache.get(logs).send({ embeds: [logsembed] });
 
 });
 
@@ -82,30 +93,43 @@ client.on("roleDelete", async role => {
     const user = await role.guild.fetchAuditLogs({
         type: 'ROLE_DELETE'
     }).then(audit => audit.entries.first())
+
     const entry = user.executor
+    let trustedusers = db.get(`trustedusers_${role.guild.id}`)
+    let logs = db.get(`acitonslogs_${role.guild.id}`)
+
+    if (trustedusers && trustedusers.find(find => find.user == entry.id)) {
+        if (logs) {
+            let trustedac = new Discord.MessageEmbed()
+                .setColor('#85db61')
+                .setTitle(`<:check:923151545401479179> ${entry.tag} Deleted An Role But He/She Is In Trusted Users`)
+            return client.channels.cache.get(logs).send({ embeds: [trustedac] });
+        }
+    }
+
     let author = db.get(`executer_${role.guild.id}_${entry.id}_roledelete`)
     let limts = db.get(`roledeletelimt_${role.guild.id}`)
+
     if (limts === null) {
-        return console.log('shit');
+        let nolimit = new Discord.MessageEmbed()
+            .setColor('#f67975')
+            .setTitle(`<:ignore:923151545569267752> ${entry.tag} Deleted A Role But No Limit Found To Punish`)
+        return client.channels.cache.get(logs).send({ embeds: [nolimit] });
     }
-    let trustedusers = db.get(`trustedusers_${role.guild.id}`)
-    if (trustedusers && trustedusers.find(find => find.user == entry.id)) {
-        return console.log('Its Trusted User');
-    }
-    let logs = db.get(`acitonslogs_${role.guild.id}`)
-    if (author > limts) {
-        db.delete(`executer_${role.guild.id}_${entry.id}`)
-        console.log('trying to ban the user..')
+
+    if (author >= limts) {
+        db.delete(`executer_${role.guild.id}_${entry.id}_roledelete`)
         role.guild.members.ban(entry.id)
         let logsembed = new Discord.MessageEmbed()
-            .setTitle(`${entry.tag} was trying to raid but failed miserabely! [Breaking Roles Delete Limts]`)
-        return client.channels.cache.get(logs).send(logsembed);
+            .setColor('#00008b')
+            .setTitle(`<:perm:923904697423777792> ${entry.tag} was trying to raid but failed miserabely [Breaking Roles Delete Limits]`)
+        return client.channels.cache.get(logs).send({ embeds: [logsembed] });
     }
-    db.add(`executer_${role.guild.id}_${entry.id}_roledelete`, 1)
-    let warn = db.get(`executer_${role.guild.id}_${entry.id}_roledelete`)
-    let logsembed = new Discord.MessageEmbed()
 
-        .setTitle(`${entry.tag} Is Deleting Roles.. [${warn || 0}/${author || 0}]`)
+    db.add(`executer_${role.guild.id}_${entry.id}_roledelete`, 1)
+    let logsembed = new Discord.MessageEmbed()
+        .setColor('RED')
+        .setTitle(`<:erorr:878139495764090880> ${entry.tag} Is Deleting Role [${author || 0} /${limts || 0}]`)
     client.channels.cache.get(logs).send(logsembed)
 });
 
@@ -113,63 +137,91 @@ client.on("channelCreate", async channel => {
     const user = await channel.guild.fetchAuditLogs({
         type: 'CHANNEL_CREATE'
     }).then(audit => audit.entries.first())
+
     const entry = user.executor
-    let trustedusers = db.get(`trustedusers_${channel.guild.id}`)
+    let trustedusers = db.get(`trustedusers_${role.guild.id}`)
+    let logs = db.get(`acitonslogs_${role.guild.id}`)
+
     if (trustedusers && trustedusers.find(find => find.user == entry.id)) {
-        return console.log('Its Trusted User');
+        if (logs) {
+            let trustedac = new Discord.MessageEmbed()
+                .setColor('#85db61')
+                .setTitle(`<:check:923151545401479179> ${entry.tag} Created An Channel But He/She Is In Trusted Users`)
+            return client.channels.cache.get(logs).send({ embeds: [trustedac] });
+        }
     }
+
     let author = db.get(`executer_${channel.guild.id}_${entry.id}_channelcreate`)
     let limts = db.get(`channelcreatelimts_${channel.guild.id}`)
-    if (limts === null) {
-        return console.log('shit');
-    }
-    let logs = db.get(`acitonslogs_${channel.guild.id}`)
-    if (author > limts) {
-        db.delete(`executer_${channel.guild.id}_${entry.id}`)
-        console.log('trying to ban the user..')
-        channel.guild.members.ban(entry.id)
-        let logsembed = new Discord.MessageEmbed()
-            .setTitle(`${entry.tag} was trying to raid but failed miserabely! [Breaking Channel Create Limts]`)
-        return client.channels.cache.get(logs).send(logsembed);
-    }
-    db.add(`executer_${channel.guild.id}_${entry.id}_channelcreate`, 1)
-    let warn = db.get(`executer_${channel.guild.id}_${entry.id}_channelcreate`)
-    let logsembed = new Discord.MessageEmbed()
 
-        .setTitle(`${entry.tag} Is Creating channel.. [${warn || 0}/${author || 0}]`)
-    client.channels.cache.get(logs).send(logsembed)
+    if (limts === null) {
+        let nolimit = new Discord.MessageEmbed()
+            .setColor('#f67975')
+            .setTitle(`<:ignore:923151545569267752> ${entry.tag} Created A Channel But No Limit Found To Punish`)
+        return client.channels.cache.get(logs).send({ embeds: [nolimit] });
+    }
+
+    if (author >= limts) {
+        db.delete(`executer_${channel.guild.id}_${entry.id}_channelcreate`)
+        role.guild.members.ban(entry.id)
+        let logsembed = new Discord.MessageEmbed()
+            .setColor('#00008b')
+            .setTitle(`<:perm:923904697423777792> ${entry.tag} was trying to raid but failed miserabely [Breaking Channel Create Limits]`)
+        return client.channels.cache.get(logs).send({ embeds: [logsembed] });
+    }
+
+    db.add(`executer_${channel.guild.id}_${entry.id}_channelcreate`, 1)
+    let logsembed = new Discord.MessageEmbed()
+        .setColor('RED')
+        .setTitle(`<:erorr:878139495764090880> ${entry.tag} Is Creating Channel [${author || 0} /${limts || 0}]`)
+    return client.channels.cache.get(logs).send({ embeds: [logsembed] });
+
 });
 
 client.on("channelDelete", async channel => {
     const user = await channel.guild.fetchAuditLogs({
         type: 'CHANNEL_DELETE'
     }).then(audit => audit.entries.first())
+
     const entry = user.executor
-    let trustedusers = db.get(`trustedusers_${channel.guild.id}`)
+    let trustedusers = db.get(`trustedusers_${role.guild.id}`)
+    let logs = db.get(`acitonslogs_${role.guild.id}`)
+
     if (trustedusers && trustedusers.find(find => find.user == entry.id)) {
-        return console.log('Its Trusted User');
+        if (logs) {
+            let trustedac = new Discord.MessageEmbed()
+                .setColor('#85db61')
+                .setTitle(`<:check:923151545401479179> ${entry.tag} Deleted An Channel But He/She Is In Trusted Users`)
+            return client.channels.cache.get(logs).send({ embeds: [trustedac] });
+        }
     }
+
     let author = db.get(`executer_${channel.guild.id}_${entry.id}_channeldelete`)
     let limts = db.get(`channeldeletelimts_${channel.guild.id}`)
-    if (limts === null) {
-        return console.log('shit');
-    }
-    let logs = db.get(`acitonslogs_${channel.guild.id}`)
-    if (author > limts) {
-        db.delete(`executer_${channel.guild.id}_${entry.id}`)
-        console.log('trying to ban the user..')
-        channel.guild.members.ban(entry.id)
-        let logsembed = new Discord.MessageEmbed()
-            .setTitle(`${entry.tag} was trying to raid but failed miserabely! [Breaking Delete Create Limts]`)
-        return client.channels.cache.get(logs).send(logsembed);
-    }
-    db.add(`executer_${channel.guild.id}_${entry.id}_channeldelete`, 1)
-    let warn = db.get(`executer_${channel.guild.id}_${entry.id}_channeldelete`)
-    let logsembed = new Discord.MessageEmbed()
 
-        .setTitle(`${entry.tag} Is Deleting channel.. [${warn || 0}/${author || 0}]`)
-    client.channels.cache.get(logs).send(logsembed)
+    if (limts === null) {
+        let nolimit = new Discord.MessageEmbed()
+            .setColor('#f67975')
+            .setTitle(`<:ignore:923151545569267752> ${entry.tag} Deleted A Channel But No Limit Found To Punish`)
+        return client.channels.cache.get(logs).send({ embeds: [nolimit] });
+    }
+
+    if (author >= limts) {
+        db.delete(`executer_${channel.guild.id}_${entry.id}_channeldelete`)
+        role.guild.members.ban(entry.id)
+        let logsembed = new Discord.MessageEmbed()
+            .setColor('#00008b')
+            .setTitle(`<:perm:923904697423777792> ${entry.tag} was trying to raid but failed miserabely [Breaking Channel Delete Limits]`)
+        return client.channels.cache.get(logs).send({ embeds: [logsembed] });
+    }
+
+    db.add(`executer_${channel.guild.id}_${entry.id}_channeldelete`, 1)
+    let logsembed = new Discord.MessageEmbed()
+        .setColor('RED')
+        .setTitle(`<:erorr:878139495764090880> ${entry.tag} Is Deleting Channel [${author || 0} /${limts || 0}]`)
+    return client.channels.cache.get(logs).send({ embeds: [logsembed] });
 });
+
 client.on("guildMemberRemove", async member => {
     const entry1 = await member.guild
         .fetchAuditLogs()
@@ -181,32 +233,47 @@ client.on("guildMemberRemove", async member => {
             })
             .then(audit => audit.entries.first());
         const entry = entry2.executor;
-        let trustedusers = db.get(`trustedusers_${member.guild.id}`)
+
+        let trustedusers = db.get(`trustedusers_${role.guild.id}`)
+        let logs = db.get(`acitonslogs_${role.guild.id}`)
+
         if (trustedusers && trustedusers.find(find => find.user == entry.id)) {
-            return console.log('Its Trusted User');
+            if (logs) {
+                let trustedac = new Discord.MessageEmbed()
+                    .setColor('#85db61')
+                    .setTitle(`<:check:923151545401479179> ${entry.tag} Kicked An Member But He/She Is In Trusted Users`)
+                return client.channels.cache.get(logs).send({ embeds: [trustedac] });
+            }
         }
+
         let author = db.get(`executer_${member.guild.id}_${entry.id}_kicklimts`)
         let limts = db.get(`kicklimts_${member.guild.id}`)
+
         if (limts === null) {
-            return console.log('shit');
+            let nolimit = new Discord.MessageEmbed()
+                .setColor('#f67975')
+                .setTitle(`<:ignore:923151545569267752> ${entry.tag} Kicked An Member But No Limit Found To Punish`)
+            return client.channels.cache.get(logs).send({ embeds: [nolimit] });
         }
-        let logs = db.get(`acitonslogs_${member.guild.id}`)
-        if (author > limts) {
-            db.delete(`executer_${member.guild.id}_${entry.id}`)
-            console.log('trying to ban the user..')
-            channel.guild.members.ban(entry.id)
+
+        if (author >= limts) {
+            db.delete(`executer_${member.guild.id}_${entry.id}_kicklimts`)
+            role.guild.members.ban(entry.id)
             let logsembed = new Discord.MessageEmbed()
-                .setTitle(`${entry.tag} was trying to raid but failed miserabely! [Breaking Kicking Members Limts]`)
-            return client.channels.cache.get(logs).send(logsembed);
+                .setColor('#00008b')
+                .setTitle(`<:perm:923904697423777792> ${entry.tag} was trying to raid but failed miserabely [Breaking Kick Limits]`)
+            return client.channels.cache.get(logs).send({ embeds: [logsembed] });
         }
-        db.add(`executer_${member.guild.id}_${entry.id}_kicklimts`, 1)
-        let warn = db.get(`executer_${member.guild.id}_${entry.id}_kicklimts`)
-        let logsembed = new Discord.MessageEmbed()
-            .setTitle(`${entry.tag} Is Kicking Members.. [${warn || 0}/${author || 0}]`)
-        client.channels.cache.get(logs).send(logsembed)
 
     }
+    db.add(`executer_${member.guild.id}_${entry.id}_kicklimts`, 1)
+    let logsembed = new Discord.MessageEmbed()
+        .setColor('RED')
+        .setTitle(`<:erorr:878139495764090880> ${entry.tag} Is Kicking Memeber [${author || 0} /${limts || 0}]`)
+    return client.channels.cache.get(logs).send({ embeds: [logsembed] });
+
 })
+
 client.on("guildMemberRemove", async member => {
     const entry1 = await member.guild
         .fetchAuditLogs()
@@ -218,29 +285,43 @@ client.on("guildMemberRemove", async member => {
             })
             .then(audit => audit.entries.first());
         const entry = entry2.executor;
-        let trustedusers = db.get(`trustedusers_${member.guild.id}`)
+
+        let trustedusers = db.get(`trustedusers_${role.guild.id}`)
+        let logs = db.get(`acitonslogs_${role.guild.id}`)
+
         if (trustedusers && trustedusers.find(find => find.user == entry.id)) {
-            return console.log('Its Trusted User');
+            if (logs) {
+                let trustedac = new Discord.MessageEmbed()
+                    .setColor('#85db61')
+                    .setTitle(`<:check:923151545401479179> ${entry.tag} Banned An Member But He/She Is In Trusted Users`)
+                return client.channels.cache.get(logs).send({ embeds: [trustedac] });
+            }
         }
+
         let author = db.get(`executer_${member.guild.id}_${entry.id}_banlimts`)
         let limts = db.get(`banlimts_${member.guild.id}`)
+
         if (limts === null) {
-            return console.log('shit');
+            let nolimit = new Discord.MessageEmbed()
+                .setColor('#f67975')
+                .setTitle(`<:ignore:923151545569267752> ${entry.tag} Banned A Member But No Limit Found To Punish`)
+            return client.channels.cache.get(logs).send({ embeds: [nolimit] });
         }
-        let logs = db.get(`acitonslogs_${member.guild.id}`)
-        if (author > limts) {
-            db.delete(`executer_${member.guild.id}_${entry.id}`)
-            console.log('trying to ban the user..')
-            member.guild.members.ban(entry.id)
+
+        if (author >= limts) {
+            db.delete(`executer_${member.guild.id}_${entry.id}_banlimts`)
+            role.guild.members.ban(entry.id)
             let logsembed = new Discord.MessageEmbed()
-                .setTitle(`${entry.tag} was trying to raid but failed miserabely! [Breaking Banning Members Limts]`)
-            return client.channels.cache.get(logs).send(logsembed);
+                .setColor('#00008b')
+                .setTitle(`<:perm:923904697423777792> ${entry.tag} was trying to raid but failed miserabely [Breaking Ban Limits]`)
+            return client.channels.cache.get(logs).send({ embeds: [logsembed] });
         }
+
         db.add(`executer_${member.guild.id}_${entry.id}_banlimts`, 1)
-        let warn = db.get(`executer_${member.guild.id}_${entry.id}_banlimts`)
         let logsembed = new Discord.MessageEmbed()
-            .setTitle(`${entry.tag} Is Banning Members.. [${warn || 0}/${author || 0}]`)
-        client.channels.cache.get(logs).send(logsembed)
+            .setColor('RED')
+            .setTitle(`<:erorr:878139495764090880> ${entry.tag} Is Banning Member [${author || 0} /${limts || 0}]`)
+        return client.channels.cache.get(logs).send({ embeds: [logsembed] });
 
     }
 })
